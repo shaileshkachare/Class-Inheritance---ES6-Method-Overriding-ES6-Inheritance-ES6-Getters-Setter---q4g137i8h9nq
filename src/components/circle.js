084bc62852100d1ded82a6871cc7c6736f0d4cb4
shaileshkachare <shaileshkachare2@gmail.com>
2023-02-09 12:56:14 +0530

    Assignment

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,16 @@
 // import using require
 
 // declare class
+var Shape = require('./shape.js');
 
+class circle extends Shape{
+    constructor(color) {
+        super();
 
+    }
+    CalculateArea(){
+
+    }
+}
 // export class using module.exports
+module.exports = circle;
